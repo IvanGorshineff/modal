96755e10341e577a8d4dca3a7b618e5577d51056
IvanGorshineff <103030246+IvanGorshineff@users.noreply.github.com>
2023-11-27 10:11:49 +0300

    add modal window

---
+++ js/index.js
@@ -0,0 +1,8 @@
+document.getElementById('open-modal_btn').addEventListener('click', function () {
+  document.getElementById('my-modal').classList.add('open')
+})
+
+document.getElementById('close-my-modal_btn').addEventListener('click', function () {
+  document.getElementById('my-modal').classList.remove('open')
+})
+
